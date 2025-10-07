9983fbf723f05fc7dae4c9b6e7a7f0ac206aadf9
ayagah <jacksonochieng1540@gmail.com>
2025-10-07 21:42:52 +0300

    urls update

--- Hudhuria/urls.py
+++ Hudhuria/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from . import views
+from attendance import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
